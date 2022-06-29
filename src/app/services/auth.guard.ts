9fefc45cdfab0221d22a506cd0ce1cc68da1fee3
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-06-29 09:43:30 +0530

    Remove : Comments

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -10,13 +10,6 @@ export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) { }
   canActivate(): boolean {
 
-    // if (this.auth.logIn()) {
-
-    //   this.router.navigate['/todo-dashboard']
-    //   return true
-    // }
-    // return false
-
     let l = localStorage.getItem('login-state')!
     let p = JSON.parse(l)
 
